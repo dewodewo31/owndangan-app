ac507631d255430d33e10b3cc5edfe2a8d26cdf8
dewodewo31 <dewodewo3104@gmail.com>
2026-08-20 21:00:58 +0800

    chore(frontend): add jest test infrastructure

---
+++ frontend/jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/src/**/*.test.{ts,tsx}'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react-jsx',
+        },
+      },
+    ],
+  },
+};
